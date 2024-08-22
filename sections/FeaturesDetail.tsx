4cbecf4691b3c9a3caf0f99eaa9915207a962fc9
Daniel Fedatto <daniel.fedatto@gmail.com>
2024-08-22 02:39:27 -0300

    feat: :sparkles: Adding FeatureDetails sections

---
+++ sections/FeaturesDetail.tsx
@@ -0,0 +1,51 @@
+import type { ImageWidget, Color } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
+import ListCarousel  from "site/islands/ListCarousel.tsx";
+
+export interface Section {
+    content?: {
+        title?: string;
+        backgroundColor?: Color;
+    };
+}
+export interface FeaturesDetail {
+    content?: {
+        image?: ImageWidget;
+        icon?: ImageWidget;
+        text?: string;
+    }[];
+}
+
+export interface SliderSetup {
+    /**
+    * @title Autoplay
+    * @description Defines if the carousel should start autoplaying (default: true)
+    */
+    autoplay?: boolean;
+    /**
+    * @title Interval
+    * @description Time (in seconds) between slide changes (default: 3.5)
+    */
+    interval?: number;
+    /** @description Content alignment */
+    layout?: "left" | "right";
+}
+
+export interface Props {
+    section?: Section[];
+    content?: FeaturesDetail[];
+    setup?: SliderSetup[];
+}
+
+function FeaturesDetail(
+    { section, content, setup }: { section: Section, content: FeaturesDetail; setup: SliderSetup },
+){
+    const { title, backgroundColor } = section.content;
+    return (
+        <div class="py-24" style={{ backgroundColor: backgroundColor }}>
+            <h1 class="md:text-5xl font-semibold">{title}</h1>
+        </div>
+    );
+}
+
+export default FeaturesDetail;
